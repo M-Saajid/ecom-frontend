3494faffbcd3e231da1222a9596eb91eaf7fb465
Saajid <saajid@gapstars.net>
2022-03-22 20:33:18 +0530

    quality updated

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { useAuth } from "./auth";
-
+import { useNavigate } from "react-router-dom";
 import { Navigate } from "react-router";
 export const RequireAuth = ({ children }) => {
   const auth = useAuth();
-  
+  const navigate = useNavigate();
 
   if (!auth.user) {
     // history.push("/");

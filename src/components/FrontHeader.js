0be5210cd7e32b559163706d5e4cc4199e730b98
Saajid <saajid@gapstars.net>
2022-03-24 22:28:14 +0530

    every thing is updated

--- src/components/FrontHeader.js
+++ src/components/FrontHeader.js
@@ -12,8 +12,14 @@ function FrontHeader() {
   console.log("local storage user", userName);
   const auth = useAuth();
 
-  const login = () => {
-    navigate("/login");
+  //remove the underline where represent as link
+  const navLink = ({ isActive }) => {
+    return {
+      textDecoration: isActive ? "none" : "none",
+      color: isActive
+        ? "rgba(255, 255, 255, 0.836)"
+        : "rgba(255, 255, 255, 0.836)"
+    };
   };
 
   //loging out user and clear all caches
@@ -40,7 +46,7 @@ function FrontHeader() {
       <div className="auth__Option">
         <div className="Signin">
           {!userName ? (
-            <NavLink to={"/login"}>
+            <NavLink style={navLink} to={"/login"}>
               <h4>Sign in </h4>
             </NavLink>
           ) : (
@@ -49,7 +55,7 @@ function FrontHeader() {
         </div>
         <div className="create__Account">
           {/* verify the user as admin or the customer */}
-          <NavLink to={"/addminview"}>
+          <NavLink style={navLink} to={"/addminview"}>
             {!userName ? <h4>Create an account</h4> : <h4>{userName}</h4>}
           </NavLink>
         </div>
--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { useAuth } from "./auth";
-
-import { Navigate } from "react-router";
 import Login from "./Login";
 import AdminItems from "./AdminItems";
+import { useAuth } from "./auth";
+
 export const RequireAuth = ({ children }) => {
   const userName = localStorage.getItem("user");
-
   const auth = useAuth();
-
   if (userName === process.env.REACT_APP_ADMIN) {
     return <AdminItems />;
-    // return <Redirect to="/login" />
-    // navigate("/");
   } else {
     return <Login />;
   }

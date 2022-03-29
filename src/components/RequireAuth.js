b344855e8a150391859e57b6d3b3530f9201d5dd
Saajid <saajid@gapstars.net>
2022-03-29 23:25:40 +0530

    all working

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -4,22 +4,10 @@ import AdminItems from "./AdminItems";
 
 export const RequireAuth = ({ children }) => {
   const userName = localStorage.getItem("user");
-<<<<<<< HEAD
 
-||||||| 0be5210
-  const auth = useAuth();
-=======
->>>>>>> jwttocken
   if (userName === process.env.REACT_APP_ADMIN) {
     return <AdminItems />;
   } else {
     return <Login />;
   }
-<<<<<<< HEAD
-||||||| 0be5210
-  return children;
-=======
-
-  return children;
->>>>>>> jwttocken
 };
